b66a785bf5fe65ce9a49f7b900532ce44cf0857f
Kevin Stanton <kevin@sproutsocial.com>
2016-03-18 23:42:05 -0500

    display hg qtop in hg segment. TODO: move this to its own segment for separate styling

--- segments/hg.py
+++ segments/hg.py
@@ -20,20 +20,44 @@ def get_hg_status():
             has_modified_files = True
     return has_modified_files, has_untracked_files, has_missing_files
 
+def get_mq_patch():
+    NO_PATCHES="no patches applied"
+    p = subprocess.Popen(['hg', 'qtop'], stdout=subprocess.PIPE)
+    stdout = p.communicate()[0].decode('utf-8')
+
+    patch_applied = NO_PATCHES
+    for line in stdout.split('\n'):
+        l = line.rstrip()
+        if l != '':
+            patch_applied = l
+            break
+
+    if patch_applied == NO_PATCHES:
+        return ''
+    else:
+        return patch_applied
+
 def add_hg_segment(powerline):
     branch = os.popen('hg branch 2> /dev/null').read().rstrip()
     if len(branch) == 0:
         return False
+
+    extra = ''
     bg = Color.REPO_CLEAN_BG
     fg = Color.REPO_CLEAN_FG
+
+    has_mq_patch = get_mq_patch()
+    if has_mq_patch:
+        extra += '[%s]' % has_mq_patch
+
     has_modified_files, has_untracked_files, has_missing_files = get_hg_status()
     if has_modified_files or has_untracked_files or has_missing_files:
         bg = Color.REPO_DIRTY_BG
         fg = Color.REPO_DIRTY_FG
-        extra = ''
         if has_untracked_files:
-            extra += '+'
+            extra += ' +'
         if has_missing_files:
-            extra += '!'
-        branch += (' ' + extra if extra != '' else '')
+            extra += ' !'
+
+    branch += (' ' + extra if extra != '' else '')
     return powerline.append('%s ' % branch, fg, bg)
